94288bf6db121954548ed0a21a767647cf6d9dcd
tes3awy <osaid2512@gmail.com>
2022-06-15 19:48:20 +0200

    Change functions to static methods

--- conversion.py
+++ conversion.py
@@ -4,13 +4,37 @@ from struct import pack
 
 
 class Convert:
-    def __init__(self, value: str) -> None:
-        self.value = value
+    @staticmethod
+    def to_ip(__hexaddr: str, /) -> str:
+        """
+        Convert from HEX Address to IPv4
 
-    def to_ip(self) -> str:
-        hex_val = self.value.replace(".", "")
-        ip_val = int(hex_val.lower(), 16)
-        return inet_ntoa(pack(">L", ip_val))
+        Parameters
+        ----------
+        __hexaddr : str
+            HEX Address. Example: c0a80101
 
-    def to_hex(self) -> str:
-        return inet_aton(self.value).hex().lower()
+        Returns
+        -------
+        str
+            IPv4 Address
+        """
+        hex_val = __hexaddr.replace(".", "")
+        return inet_ntoa(pack(">L", int(hex_val.lower(), 16)))
+
+    @staticmethod
+    def to_hex(__ipaddr: str, /) -> str:
+        """
+        Convert from IPv4 to HEX Address
+
+        Parameters
+        ----------
+        __ipaddr : str
+            IPv4 Address. Example: 192.168.1.1
+
+        Returns
+        -------
+        str
+            HEX Address
+        """
+        return inet_aton(__ipaddr).hex().lower()
