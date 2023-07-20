ec94f647731aba8ba962b4049ce94bc0d4b1cdf2
Osama Abbas <osaid2512@gmail.com>
2023-07-20 21:10:28 +0300

    First commit

---
+++ __init__.py
@@ -0,0 +1,17 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+# ------------------------------------------------------------------------
+#
+# Demonstrates how to convert from IPv4 Address to HEX IP and vice versa
+#
+# (C) 2021-2022 Osama Abbas, Cairo, Egypt
+# Released under MIT License
+#
+# Filename: __init__.py
+# Version: Python 3.10.4
+# Author: Osama Abbas (oabbas2512@gmail.com)
+# Description:   This program is designed to convert from IPv4 Address to
+#                HEX IP and vice versa.
+#
+# ------------------------------------------------------------------------
